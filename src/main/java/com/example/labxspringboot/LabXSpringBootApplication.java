b7c3ce39a14ed4d3a3ef6eaa1acb2b89a4e7a0d1
hamza <hamzabenmoussa166@gmail.com>
2024-01-10 00:08:06 +0100

    Initial commit

---
+++ src/main/java/com/example/labxspringboot/LabXSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.labxspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabXSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabXSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/labxspringboot/LabXSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labxspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabXSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
