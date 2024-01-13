b973eade89e55c55a07d1f8677ce30033250241a
hamza <hamzabenmoussa166@gmail.com>
2024-01-13 17:44:11 +0100

    fixed the entity class

--- src/main/java/com/example/labxspringboot/repository/ITestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.labxspringboot.repository;
-
-import com.example.labxspringboot.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ITestRepository extends JpaRepository<Test,Long> {
-}
